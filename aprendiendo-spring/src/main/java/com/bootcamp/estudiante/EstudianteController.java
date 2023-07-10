f5113e85f9efb94cfdb10c80b165cc7f1e5a5a0d
brauliogm <brauliogm16@gmail.com>
2023-07-10 09:09:46 -0600

    Haciendo la validacion para la creacion del estudiante

--- aprendiendo-spring/src/main/java/com/bootcamp/estudiante/EstudianteController.java
+++ aprendiendo-spring/src/main/java/com/bootcamp/estudiante/EstudianteController.java
@@ -23,8 +23,8 @@ public class EstudianteController{
     }
 
     @PostMapping
-    public void createEstudiante(@RequestBody Estudiante estudiante){
-        estudianteService.createEstudiante(estudiante);
+    public void createEstudiante(@RequestBody Long estudianteId, Estudiante estudiante){
+        estudianteService.createEstudiante(estudianteId, estudiante);
     }
 
     @DeleteMapping("{id}")
--- aprendiendo-spring/src/main/java/com/bootcamp/estudiante/EstudianteService.java
+++ aprendiendo-spring/src/main/java/com/bootcamp/estudiante/EstudianteService.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
 
 @Service
 public class EstudianteService {
@@ -24,7 +25,11 @@ public class EstudianteService {
         return estudiantes;
     }
 
-    public void createEstudiante(Estudiante estudiante){
+    public void createEstudiante(Long estudianteId, Estudiante estudiante){
+
+        emailValido(estudiante);
+
+        emailRegistrado(estudianteId, estudiante);
 
         estudianteRepository.save(estudiante);
     }
@@ -46,11 +51,12 @@ public class EstudianteService {
         Estudiante estudianteExistente = estudianteRepository.findById(estudianteId)
                 .orElseThrow(() -> new NoSuchElementException("Estudiante con ese id no existe, id: " + estudianteId));
 
+        //check si el email en valido
+
+        emailValido(estudiante);
+
         //check si el email que se quiere actualizar ya existe
-        boolean emailExiste = estudianteRepository.existsByEmailAndIdIsNot(estudiante.getEmail(), estudianteId);
-        if (emailExiste) {
-            throw new IllegalArgumentException("email " + estudiante.getEmail() + " ya esta registrado");
-        }
+        emailRegistrado(estudianteId, estudiante);
 
         //Actualizar Estudiante
         estudianteExistente.setPrimerNombre(estudiante.getPrimerNombre());
@@ -67,6 +73,27 @@ public class EstudianteService {
     public Estudiante getEstudianteUnico(Long estudianteId){
 
         return estudianteRepositoryMentiras.getEstudianteUnico(estudianteId);
+    }
+
+    boolean checkValideszEmail(String email){
+        return Pattern.compile(
+                "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+                Pattern.CASE_INSENSITIVE
+        ).asPredicate().test(email);
+    }
+
+
+    private void emailValido(Estudiante estudiante) {
+        if (!checkValideszEmail((estudiante.getEmail()))) {
+            throw new IllegalArgumentException("Email " + estudiante.getEmail() + " no es valido");
+        }
+    }
+
+    private void emailRegistrado(Long estudianteId, Estudiante estudiante) {
+        boolean emailExiste = estudianteRepository.existsByEmailAndIdIsNot(estudiante.getEmail(), estudianteId);
+        if (emailExiste) {
+            throw new IllegalArgumentException("email " + estudiante.getEmail() + " ya esta registrado");
+        }
     }
 
 }
